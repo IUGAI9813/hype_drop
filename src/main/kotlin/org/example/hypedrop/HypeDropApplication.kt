b693ddd7a9c91b80f3302cac4b7cd17e56554fc0
ALEKSANDR <hhhs2332@gmail.com>
2026-04-06 22:34:06 +0900

    init: Spring Boot + Kotlin + WebFlux project

---
+++ src/main/kotlin/org/example/hypedrop/HypeDropApplication.kt
@@ -0,0 +1,11 @@
+package org.example.hypedrop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HypeDropApplication
+
+fun main(args: Array<String>) {
+    runApplication<HypeDropApplication>(*args)
+}
